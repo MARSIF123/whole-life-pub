d4b884ad5138c6e54a14890c1bd5f770cf7f21d1
MARSIF123 <maryamw561@gmail.com>
2024-07-02 15:59:27 +0500

    WIP :responsive mobile nav

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,22 +1,47 @@
-import React from 'react'
-import styles from './header.module.css'
-import image from '../../../public/Favicon.png'
-import Image from 'next/image'
-import Navbar from '../Navbar/Navbar'
-import Search from '../Search/Search'
+import React from "react";
+import styles from "./header.module.css";
+import image from "../../../public/Favicon.png";
+import Image from "next/image";
+import Navbar from "../Navbar/Navbar";
+import Search from "../Search/Search";
+import Link from "next/link";
+import MobileNavbar from "../MobileNavbar/MobileNavbar";
+import ShuffleIcon from "../Icons/ShuffleIcon";
 
 function Header() {
   return (
     <div className={styles.header}>
-      <div className={styles.logoImage}>
-        <Image src={image} className={styles.img} alt='logo-wholelifepub' priority={true} />
+      <div className={styles.mobileNav}>
+        <MobileNavbar />
       </div>
+      <Link className={styles.logoImage} href={"/"}>
+        <Image
+          src={image}
+          className={styles.img}
+          alt="logo-wholelifepub"
+          priority={true}
+        />
+      </Link>
       <div className={styles.nav}>
         <Navbar />
         <Search />
       </div>
+      <div className={styles.podcast}>
+        <ul className={styles.list}>
+          <li className={styles.navLi}>
+            <Link className={styles.link1} href={"/random"}>
+              Random
+            </Link>
+          </li>
+          <li className={styles.navLi}>
+            <Link className={styles.link2} href={"/podcost"}>
+              PODCOST
+            </Link>
+          </li>
+        </ul>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Header
+export default Header;
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,51 +1,138 @@
-import React from 'react'
-import styles from './navbar.module.css'
-import Link from 'next/link'
-import ArrowDown from '../Icons/ArrowDown'
-import ShuffleIcon from '../Icons/ShuffleIcon'
+import React from "react";
+import styles from "./navbar.module.css";
+import Link from "next/link";
+import ArrowDown from "../Icons/ArrowDown";
+import ShuffleIcon from "../Icons/ShuffleIcon";
 
 function Navbar() {
-    return (
-        <ul className={styles.nav}>
-            <li className={styles.navLi}><Link className={styles.link} href={'/style'}>GET STYLE
-                <ArrowDown /></Link>
-                <ul className={`${styles.nav} ${styles.innerNav}`} >
-                    <li><Link className={styles.innerlink} href={'/style/clothing'}>CLOTHING</Link></li>
-                    <li><Link className={styles.innerlink} href={'/style/accessories'}>ACCESSORIES</Link></li>
-                    <li><Link className={styles.innerlink} href={'/style/facial-hair'}>FACIAL HAIRr</Link></li>
-                    <li><Link className={styles.innerlink} href={'/style/ties'}>TIES</Link></li></ul>
-
-            </li>
-            <li className={styles.navLi}><Link className={styles.link}
-                href={'/health-fitness'} >
-                GET STRONG
-                <ArrowDown /></Link>
-                <ul className={`${styles.nav} ${styles.innerNav}`} >
-                    <li><Link className={styles.innerlink} href={'/health-fitness/fitness'}>Fitness</Link></li>
-                    <li><Link className={styles.innerlink} href={'/health-fitness/health'}>Health</Link></li>
-                    <li><Link className={styles.innerlink} href={'/health-fitness/program-review'}>Program Review</Link></li></ul>
-
-            </li>
-            <li className={styles.navLi}><Link className={styles.link} href={'/people'} >GET SOCIAL
-                <ArrowDown /></Link>
-                <ul className={`${styles.nav} ${styles.innerNav}`} >
-                    <li><Link className={styles.innerlink} href={'/people/family'}>FAMILY</Link></li>
-                    <li><Link className={styles.innerlink} href={'/people/fatherhood'}>FATHERHOOD</Link></li>
-                    <li><Link className={styles.innerlink} href={'/people/relationships'}>RELATIONSHIPS</Link></li>
-                    <li><Link className={styles.innerlink} href={'/people/social-skills'}>SOCIAL SKILLS</Link></li></ul>
-            </li>
-            <li className={styles.navLi}><Link className={styles.link} href={'/skills'} >GET SKILLED
-                <ArrowDown /></Link>
-                <ul className={`${styles.nav} ${styles.innerNav}`} >
-                    <li><Link className={styles.innerlink} href={'/skills/how-to'}>HOW TO</Link></li>
-                    <li><Link className={styles.innerlink} href={'/skills/manly-know-how'}>MANLY KNOW-HOW</Link></li>
-                    <li><Link className={styles.innerlink} href={'/skills/outdoor-survival'}>OUTDOOR/SURVIVAL</Link></li></ul>
-            </li>
-            <li className={styles.navLi}><Link className={styles.link} href={'/podcost'} >PODCOST</Link></li>
-            <li className={styles.navLi}><Link className={styles.link} href={'/collections/books'} >BOOKS</Link></li>
-            <li className={styles.navLi}><Link className={styles.link} href={'/random'}>RANDOM<ShuffleIcon /></Link></li>
+  return (
+    <ul className={styles.nav}>
+      <li className={styles.navLi}>
+        <Link className={styles.link} href={"/style"}>
+          GET STYLE
+          <ArrowDown />
+        </Link>
+        <ul className={`${styles.nav} ${styles.innerNav}`}>
+          <li>
+            <Link className={styles.innerlink} href={"/style/clothing"}>
+              CLOTHING
+            </Link>
+          </li>
+          <li>
+            <Link className={styles.innerlink} href={"/style/accessories"}>
+              ACCESSORIES
+            </Link>
+          </li>
+          <li>
+            <Link className={styles.innerlink} href={"/style/facial-hair"}>
+              FACIAL HAIRr
+            </Link>
+          </li>
+          <li>
+            <Link className={styles.innerlink} href={"/style/ties"}>
+              TIES
+            </Link>
+          </li>
         </ul>
-    )
+      </li>
+      <li className={styles.navLi}>
+        <Link className={styles.link} href={"/health-fitness"}>
+          GET STRONG
+          <ArrowDown />
+        </Link>
+        <ul className={`${styles.nav} ${styles.innerNav}`}>
+          <li>
+            <Link className={styles.innerlink} href={"/health-fitness/fitness"}>
+              Fitness
+            </Link>
+          </li>
+          <li>
+            <Link className={styles.innerlink} href={"/health-fitness/health"}>
+              Health
+            </Link>
+          </li>
+          <li>
+            <Link
+              className={styles.innerlink}
+              href={"/health-fitness/program-review"}
+            >
+              Program Review
+            </Link>
+          </li>
+        </ul>
+      </li>
+      <li className={styles.navLi}>
+        <Link className={styles.link} href={"/people"}>
+          GET SOCIAL
+          <ArrowDown />
+        </Link>
+        <ul className={`${styles.nav} ${styles.innerNav}`}>
+          <li>
+            <Link className={styles.innerlink} href={"/people/family"}>
+              FAMILY
+            </Link>
+          </li>
+          <li>
+            <Link className={styles.innerlink} href={"/people/fatherhood"}>
+              FATHERHOOD
+            </Link>
+          </li>
+          <li>
+            <Link className={styles.innerlink} href={"/people/relationships"}>
+              RELATIONSHIPS
+            </Link>
+          </li>
+          <li>
+            <Link className={styles.innerlink} href={"/people/social-skills"}>
+              SOCIAL SKILLS
+            </Link>
+          </li>
+        </ul>
+      </li>
+      <li className={styles.navLi}>
+        <Link className={styles.link} href={"/skills"}>
+          GET SKILLED
+          <ArrowDown />
+        </Link>
+        <ul className={`${styles.nav} ${styles.innerNav}`}>
+          <li>
+            <Link className={styles.innerlink} href={"/skills/how-to"}>
+              HOW TO
+            </Link>
+          </li>
+          <li>
+            <Link className={styles.innerlink} href={"/skills/manly-know-how"}>
+              MANLY KNOW-HOW
+            </Link>
+          </li>
+          <li>
+            <Link
+              className={styles.innerlink}
+              href={"/skills/outdoor-survival"}
+            >
+              OUTDOOR/SURVIVAL
+            </Link>
+          </li>
+        </ul>
+      </li>
+      <li className={styles.navLi}>
+        <Link className={styles.link} href={"/podcost"}>
+          PODCOST
+        </Link>
+      </li>
+      <li className={styles.navLi}>
+        <Link className={styles.link} href={"/collections/books"}>
+          BOOKS
+        </Link>
+      </li>
+      <li className={styles.navLi}>
+        <Link className={styles.link} href={"/random"}>
+          RANDOM
+          <ShuffleIcon />
+        </Link>
+      </li>
+    </ul>
+  );
 }
 
-export default Navbar
+export default Navbar;
